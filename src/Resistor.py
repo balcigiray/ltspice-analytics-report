fcc0f969b88204d3a1501405190538d70b70fc88
Giray Balcı <giray.balci@arcelik.com>
2021-07-14 11:41:59 +0300

    Hard coded measurement commands are excluded in component definitions. Additional measurement commands are added.

--- src/Resistor.py
+++ src/Resistor.py
@@ -5,33 +5,62 @@ Created on Fri Jul  9 12:50:03 2021
 @author: Giray Balcı
 """
 
-class Resistor:
-    measVRMS0 = ".meas %s_VRMS RMS V(%s)\n"
-    measVRMS = ".meas %s_VRMS RMS V(%s,%s)\n"
-    measVMAX0 = ".meas %s_VMAX MAX V(%s)\n"
-    measVMAX = ".meas %s_VMAX MAX V(%s,%s)\n"
-    
-    
+class Resistor:    
     def __init__(self, name, node1, node2, value):
         self.name = name
         self.node1 = node1
         self.node2 = node2
         self.value = value
+        self.type = "resistor"
+        self.numberOfMeas = 0
         
-    def createMeasCommand(self):
+        
+    def createMeasCommandVol(self, measCommand):
         measText = ""
         
         if(self.node2 == "0"): #if node is connected to ground
-            measText = measText + self.measVRMS0 % (self.name, self.node1)
-            measText = measText + self.measVMAX0 % (self.name, self.node1)
+            measText = measText + measCommand % (self.name) + "(%s)\n" % (self.node1)
         else:
-            measText = measText + self.measVRMS % (self.name, self.node1, self.node2)            
-            measText = measText + self.measVMAX % (self.name, self.node1, self.node2)            
+            measText = measText + measCommand % (self.name) + "(%s,%s)\n" % (self.node1, self.node2)          
+  
+        return measText
+
+
+    def createMeasCommandCur(self, measCommand):
+        measText = ""
+        measText = measText + measCommand % (self.name) + "(%s)\n" % (self.name)
         
         return measText
+    
+    
+    def createMeasCommandPow(self, measCommand):
+        measText = ""
+        
+        if(self.node2 == "0"): #if node is connected to ground
+            measText = measText + measCommand % (self.name) + "(%s)*I(%s)\n" % (self.node1, self.name)
+        else:
+            measText = measText + measCommand % (self.name) + "(%s,%s)*I(%s)\n" % (self.node1, self.node2, self.name)          
+  
+        return measText
+    
     
     def addMeasurement(self, keyword, measurement):
         if(keyword == "VRMS"):
             self.vrms = measurement
+            self.numberOfMeas += 1
         if(keyword == "VMAX"):
             self.vmax = measurement
+            self.numberOfMeas += 1
+        if(keyword == "IRMS"):
+            self.irms = measurement
+            self.numberOfMeas += 1
+        if(keyword == "IMAX"):
+            self.imax = measurement     
+            self.numberOfMeas += 1
+        if(keyword == "PAVG"):
+            self.pavg = measurement
+            self.numberOfMeas += 1
+        if(keyword == "PMAX"):
+            self.pmax = measurement                 
+            self.numberOfMeas += 1
+        
